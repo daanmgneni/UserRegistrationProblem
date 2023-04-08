9fb2c31453f5cab060371675eae5571b72fdc08c
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-08 07:59:23 +0530

    Update UserRegistrationCode.cs

--- UserRegistrationCode.cs
+++ UserRegistrationCode.cs
@@ -11,9 +11,26 @@ namespace UserRegistrationProgram
     public class UserRegistrationCode
     {
         public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Email = "^[a-z0-9._+-]+@[a-z0-9.-]+\\.[a-z.]{2,}$";
+
+        //CHECK 1ST NAME
         public bool ValidateFName(string FName)
         {
             return Regex.IsMatch(FName, FirstName); 
+        }
+       
+
+        //CHECK LAST NAME
+        public bool ValidateLName(string LName)
+        {
+            return Regex.IsMatch(LName, FirstName);
+        }
+
+        //CHECK EMAIL
+        public bool ValidateEmail(string MailId)
+        {
+            return Regex.IsMatch(MailId, Email);
         }
     }
 }
