d6a3a8e32f5528646da559193a25a7ad30de905a
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-08 07:48:20 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace UserRegistrationProblem
+namespace UserRegistrationProgram
 {
     class Program
-    {
-        public static void Main(string[] args)
+    {public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Problem");
+            Console.WriteLine("Welcome to User Registration Page!!");
+            Console.WriteLine("********************************");
+            UserRegistrationCode obj = new UserRegistrationCode();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine(obj.ValidateFName(FName));
         }
     }
 }
