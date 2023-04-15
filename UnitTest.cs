8e59b68c39da173eb08ab95fece6c69aa7dc6d10
daanmgneni <94113582+daanmgneni@users.noreply.github.com>
2023-04-15 09:18:44 +0530

    Update UnitTest.cs

--- UnitTest.cs
+++ UnitTest.cs
@@ -4,10 +4,8 @@ namespace TestProject2
     [TestClass]
     public class UnitTest1
     {
+        //HAPPY TEST CASE
         [TestMethod]
-        
-            //HAPPY TEST CASE
-
             public void ValidateUserEntry_ValidInputs_ReturnsValidMessages()
             {
                 //ARRNGE
@@ -33,7 +31,7 @@ namespace TestProject2
                 Assert.AreEqual("Valid Phone Number!", phoneValidationResult);
                 Assert.AreEqual("Valid Password!", passwordValidationResult);
             }
-
+        /*
             //SAD TEST CASE
             [TestMethod]
             public void ValidateUserEntry_ValidInputs_ReturnsInValidMessages()
@@ -44,7 +42,6 @@ namespace TestProject2
                 string email = "aakanks.com";
                 string phone = "911234567890";
                 string password = "Ab234";
-
                 //ACT
                 ValidationCode validationCode = new ValidationCode();
                 string firstNameValidationResult = validationCode.ValidateFirstName(firstName);
@@ -52,17 +49,80 @@ namespace TestProject2
                 var emailValidationResult = validationCode.ValidateEmail(email);
                 var phoneValidationResult = validationCode.ValidatePhone(phone);
                 var passwordValidationResult = validationCode.ValidatePassword(password);
-
                 //ASSERT
-                // Assert.AreEqual("Valid first name!", firstNameValidationResult);
-
+                
                 Assert.AreEqual("Invalid first name. Please enter a first name starting with a capital letter and at least 3 characters.", firstNameValidationResult);
                 Assert.AreEqual("Invalid last name. Please enter a last name starting with a capital letter and at least 3 characters.", lastNameValidationResult);
                 Assert.AreEqual("Invalid email. Please enter a valid email address.", emailValidationResult);
                 Assert.AreEqual("Invalid Phone Number. Please enter a valid Phone Number.", phoneValidationResult);
                 Assert.AreEqual("Invalid Password. Please enter a valid Password.", passwordValidationResult);
             }
+        */
 
-        
+        //TEST CASE FOR EMAIL
+        [TestMethod]
+        public void ValidateUserEntry_ValidInputs_ReturnsValidEmail()
+        {
+            //ARRNGE
+           
+            string email2 = "aakanksha@gmail.com";
+          
+
+            //ACT
+            ValidationCode validationCode = new ValidationCode();
+
+            var AllEmailValidationResult = validationCode.ValidateAllEmailSample(email2);
+
+            //ASSERT
+           
+            Assert.AreEqual("your email passed validation", AllEmailValidationResult);
+           
+        }
+
+        [TestMethod]
+        public void ValidateUserEntry_InvalidInputs_ThrowsCustomExceptions()
+        {
+            // Arrange
+            string firstName = "Aa";
+            string lastName = "dey";
+            string email = "aakanks.com";
+            string phone = "911234567890";
+            string password = "Ab234";
+            ValidationCode validationCode = new ValidationCode();
+
+            // Assert
+            Assert.ThrowsException<InvalidFirstNameException>(() =>
+            {
+                // Act
+                validationCode.ValidateFirstName(firstName);
+            });
+
+            Assert.ThrowsException<InvalidLastNameException>(() =>
+            {
+                // Act
+                validationCode.ValidateLastName(lastName);
+            });
+
+            Assert.ThrowsException<InvalidEmailException>(() =>
+            {
+                // Act
+                validationCode.ValidateEmail(email);
+            });
+
+            Assert.ThrowsException<InvalidPhoneException>(() =>
+            {
+                // Act
+                validationCode.ValidatePhone(phone);
+            });
+
+            Assert.ThrowsException<InvalidPasswordException>(() =>
+            {
+                // Act
+                validationCode.ValidatePassword(password);
+            });
+        }
+
+
+
     }
 }
